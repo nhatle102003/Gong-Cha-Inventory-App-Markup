acab481392af8583bcc80c6b6ca8c5bd19015252
nhatle102003 <nhle102003@gmail.com>
2023-12-17 10:32:28 -0600

    Change {password} to the desinated password when testing

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 
-export const mongoDBURL = "mongodb+srv://root:1234@gong-cha-inventory.uzy9ma2.mongodb.net/?retryWrites=true&w=majority"
+export const mongoDBURL = "mongodb+srv://root:{password}@gong-cha-inventory.uzy9ma2.mongodb.net/?retryWrites=true&w=majority"
