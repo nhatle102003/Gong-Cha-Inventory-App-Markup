b61febd372b82c55e68bcbbe67ab5031d4b53e03
nhatle102003 <nhle102003@gmail.com>
2023-12-17 13:47:37 -0600

    Implementing router for different pages of the page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {Route, Routes} from 'react-router-dom'
+import Home from './clients/homeClient'
+import DeleteItems from './clients/deleteItemsClient'
+import ShowItems from './clients/showItemsClient'
+import CreateItems from './clients/createItemsClient'
+import EditItems from './clients/editItemsClient'
 
 const App = () => {
   return (
-    <div className = 'bg-red-400 text-white'>App</div>
+    <Routes>
+      <Route path = '/' element = {<Home/>} ></Route>
+      <Route path = '/items/create' element = {<CreateItems/>}></Route>
+      <Route path = '/items/delete/:id' element = {<DeleteItems/>}></Route>
+      <Route path = '/items/edit/:id' element = {<EditItems/>}></Route>
+      <Route path = '/items/show/:id' element = {<ShowItems/>}></Route>
+    </Routes>
   )
 }
 
