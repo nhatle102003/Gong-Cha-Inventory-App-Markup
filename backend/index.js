38838f37d10f30ffb392e9b2f78beef9f3b30165
nhatle102003 <nhle102003@gmail.com>
2023-12-17 10:14:05 -0600

    Add update, get, and delete items method

--- backend/index.js
+++ backend/index.js
@@ -35,6 +35,76 @@ app.post('/items', async (request, reponse) => {
     }
 })
 
+//Get method to get ALL items from database 
+
+app.get('/items', async (req, res) => {
+    try{
+        const items = await Inventory.find({})
+        return res.status(200).json({
+            total: items.length,
+            data: items
+        })
+    }catch(error){
+        console.log(error.message)
+        reponse.status(500).send({message: error.message})
+    }
+})
+
+//Get method to get ONE items from database by ID
+
+app.get('/items/:id', async (req, res) => {
+    try{
+        const {id} = req.params
+        const item = await Inventory.findById(id)
+
+        return res.status(200).json({
+            item
+        })
+    }catch(error){
+        console.log(error.message)
+        reponse.status(500).send({message: error.message})
+    }
+})
+
+//Route to find a item with a unique id and update the item
+
+app.put('/items/:id', async (req, res) => {
+    try{
+        if(!req.body.name || !req.body.quantity || !req.body.metrics){
+            return reponse.status(400).send({
+                message: "Send all required fields"
+            })
+        }
+        const {id} = req.params
+        const result = await Inventory.findByIdAndUpdate(id, req.body)
+
+        if (!result){
+            return res.status(404).json({message: 'Item not found'})
+        }
+        return res.status(200).send({message: 'Item updated'})
+    }catch(error){
+        console.log(error.message)
+        res.status(500).send({message: error.message})
+    }
+
+})
+
+//route for deleting an item 
+app.delete('/items/:id', async (req, res) => {
+    try{
+        const {id} = req.params
+        const result = await Inventory.findByIdAndDelete(id)
+
+        if (!result){
+            return res.status(404).json({message: 'Item not found'})
+        }
+        return res.status(200).send({message: 'Item deleted'})
+        
+    }catch(error){
+        console.log(error.message)
+        res.status(500).send({message: error.message})
+    }   
+})
 
 mongoose
     .connect(mongoDBURL)
