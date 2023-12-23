a93b2392957e488986e214bcbc88cc4d3fa7e10d
nhatle102003 <nhle102003@gmail.com>
2023-12-23 10:41:07 -0600

    Reorgazied the clients folder

---
+++ frontend/clients/createItemsClient.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const createItemsClient = () => {
+  return (
+    <div>createItemsClient</div>
+  )
+}
+
+export default createItemsClient
---
+++ frontend/clients/showItemsClient.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const showItemsClient = () => {
+  return (
+    <div>showItemsClient</div>
+  )
+}
+
+export default showItemsClient
