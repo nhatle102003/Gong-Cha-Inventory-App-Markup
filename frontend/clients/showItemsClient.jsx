d422f9b03057604530bcb21517fdd3832ca98adb
nhatle102003 <nhle102003@gmail.com>
2024-01-05 16:00:39 -0600

    Fix object not parsing correctly

--- frontend/clients/showItemsClient.jsx
+++ frontend/clients/showItemsClient.jsx
@@ -2,54 +2,67 @@ import React, {useEffect, useState} from 'react'
 import axios from "axios"
 import { useParams } from 'react-router-dom'
 import BackButton from '../components/BackButton'
+import Spinner from '../components/Spinner'
 
 
 const showItemsClient = () => {
-  const [item, setItem] = useState({})
+  const [product, setItem] = useState({})
+  const [loading, setLoading] = useState(true)
   const { id } = useParams()
 
   useEffect(() => {
-    axios
-    .get(`http://localhost:5555/items/${id}`)
-    .then((response) => {
-        setItem(response.data)}
-        )
-    .catch((error)=> {
+    
+    console.log("useEffect called")
+    const fetchData = async () => {
+      const result = await axios
+      .get(`http://localhost:5555/items/${id}`)
+      .catch((error)=> {
         console.log(error)
-    })
+      })
+      .finally(setLoading(false))
+    if(result){
+      setItem(result.data.item)
+    }else{
+      alert("Something went wrong")
+    }
+    }
+    fetchData()
   }, [])
- 
 
   return (
     <div className = 'p-4'>
       <BackButton />
       <h1 className = "text-3x1">Show Item</h1>
-      <div className='flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4'>
+      {loading ? (
+        <Spinner/>
+      ) : (
+        <div className='flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4'>
         <div className = "my-4">
           <span className="text-xl mr-4 text-gray-500">Id</span>
-          <span>{item.item && item.item._id}</span>
+          <span>{product._id}</span>
         </div>
         <div className = "my-4">
           <span className="text-xl mr-4 text-gray-500">Name</span>
-          <span>{item.item && item.item.name}</span>
+          <span>{product.name}</span>
         </div>  
         <div className = "my-4">
           <span className="text-xl mr-4 text-gray-500">Quantity</span>
-          <span>{item.item && item.item.quantity}</span>
+          <span>{product.quantity}</span>
         </div>  
         <div className = "my-4">
           <span className="text-xl mr-4 text-gray-500">Metric</span>
-          <span>{item.item && item.item.metrics}</span>
+          <span>{product.metrics}</span>
         </div>  
         <div className = "my-4">
           <span className="text-xl mr-4 text-gray-500">Entry Date</span>
-          <span>{new Date( item.item && item.item.createdAt).toString()}</span>
+          <span>{new Date(product.createdAt).toString()}</span>
         </div>  
         <div className = "my-4">
           <span className="text-xl mr-4 text-gray-500">Updated Date</span>
-          <span>{new Date(item.item && item.item.updatedAt).toString()}</span>
+          <span>{new Date(product.updatedAt).toString()}</span>
         </div> 
       </div>
+      )}
     </div>
   )
 }
