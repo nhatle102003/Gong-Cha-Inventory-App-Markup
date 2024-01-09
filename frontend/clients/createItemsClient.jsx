868ddea5745d8697a91152e39f0bebb0273e2e4f
nhatle102003 <nhle102003@gmail.com>
2024-01-09 02:37:22 -0600

    Adding new package

--- frontend/clients/createItemsClient.jsx
+++ frontend/clients/createItemsClient.jsx
@@ -1,8 +1,79 @@
-import React from 'react'
+import React, { useState } from 'react'
+import BackButton from '../components/BackButton'
+import Spinner from '../components/Spinner'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+
 
 const createItemsClient = () => {
+  const [name, setItemName] = useState('')
+  const [quantity, setQuantity] = useState('')
+  const [metrics, setMetric] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const navigate = useNavigate()
+
+  const handleSaveItem = () => {
+    setLoading(true)
+    const data = {
+      name,
+      quantity,
+      metrics
+    }
+    axios
+    .post('http://localhost:5555/items', data)
+    .then(() => {
+      navigate('/'),
+      setLoading(false)
+    })
+    .catch((error)=> {
+      console.log(error)
+      alert("An error occured. Please check the developer log")
+    })
+    console.log(data)
+  }
+  
   return (
-    <div>createItemsClient</div>
+    <div className = 'p-4'>
+      <BackButton />
+      <h1 className = 'text-3xl my-4'>Add Item</h1>
+      {loading ? <Spinner/> : ''}
+      <div className = 'flex flex-col border 2 border-sky-400 rounded-xl w-[600px] p4 mx-auto' >
+        <div className = "my-4" >
+          <lable className = "text-xl mr-4 text-gray-500">Name</lable>
+          <input
+            type = 'text'
+            value={name}
+            onChange={(e) => setItemName(e.target.value)}
+            className = 'border-2 border-gray-500 px-4 py-2 w-full'
+          />
+        </div>
+      </div>
+      <div className = 'flex flex-col border 2 border-sky-400 rounded-xl w-[600px] p4 mx-auto' >
+        <div className = "my-4" >
+          <lable className = "text-xl mr-4 text-gray-500">Quantity</lable>
+          <input
+            type = 'text'
+            value={quantity}
+            onChange={(e) => setQuantity(e.target.value)}
+            className = 'border-2 border-gray-500 px-4 py-2 w-full'
+          />
+        </div>
+      </div>
+      <div className = 'flex flex-col border 2 border-sky-400 rounded-xl w-[600px] p4 mx-auto' >
+        <div className = "my-4" >
+          <lable className = "text-xl mr-4 text-gray-500">Metric</lable>
+          <input
+            type = 'text'
+            value={metrics}
+            onChange={(e) => setMetric(e.target.value)}
+            className = 'border-2 border-gray-500 px-4 py-2 w-full'
+          />
+        </div>
+      </div>
+      <button className = 'p-2 bg-sky-300 m-8' onClick ={handleSaveItem}>Save</button>
+    </div>
+    
   )
 }
 
