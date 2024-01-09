2f7b905158e165390e930f26b66394b484c636ae
nhatle102003 <nhle102003@gmail.com>
2024-01-09 14:12:33 -0600

    Add image holder. TODO: work on a functional user upload system

--- frontend/clients/homeClient.jsx
+++ frontend/clients/homeClient.jsx
@@ -43,25 +43,28 @@ const homeClient = () => {
         </div> 
         {load ? (<Spinner/>) : (
           <div className = 'flex justify-between'>
-            <div className = "grid grid-flow-col justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
+            <div className = "w-fit mx-auto grid grid-flow-col justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5">
               {items.map((item) => (
-                <div className = "px-4 py-3 w-72">
-                  <span class="text-black-400 mr-3 uppercase text-xs">{item.name}</span>
-                <div class="flex items-center">
-                <p class="text-lg  text-black cursor-auto my-3">{item.quantity} {item.metrics}</p>
-                </div>
-                <div className='flex justify-left gap-x-4'>
-                    <Link to={`/items/show/${item._id}`}>
-                      <BsInfoCircle className = 'text-2x1 text-green-800'/>
-                    </Link>
-                    <Link to={`/items/edit/${item._id}`}>
-                      <AiOutlineEdit className = 'text-2x1 text-yellow-600'/>
-                    </Link>
-                    <Link to={`/items/delete/${item._id}`}>
-                      <MdOutlineDelete className = 'text-2x1 text-red-600'/>
-                    </Link>
+                <div class="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
+                  <img src = "./images/holder.jpg"></img>
+                  <div className = "px-4 py-3 w-72">
+                      <span class="text-black-400 mr-3 uppercase text-xs">{item.name}</span>
+                    <div class="flex items-center">
+                    <p class="text-lg  text-black cursor-auto my-3">{item.quantity} {item.metrics}</p>
+                    </div>
+                    <div className='flex justify-left gap-x-4'>
+                        <Link to={`/items/show/${item._id}`}>
+                          <BsInfoCircle className = 'text-2x1 text-green-800 duration-500 hover:scale-105 hover:shadow-xl'/>
+                        </Link>
+                        <Link to={`/items/edit/${item._id}`}>
+                          <AiOutlineEdit className = 'text-2x1 text-yellow-600 duration-500 hover:scale-105 hover:shadow-xl'/>
+                        </Link>
+                        <Link to={`/items/delete/${item._id}`}>
+                          <MdOutlineDelete className = 'text-2x1 text-red-600 duration-500 hover:scale-105 hover:shadow-xl'/>
+                        </Link>
+                      </div>
                   </div>
-              </div>
+                </div>
               ))}
             </div>
           </div>
