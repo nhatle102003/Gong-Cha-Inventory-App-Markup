9cf065d2e6ef53235a40903501eed2ec37b8b273
nhatle102003 <nhle102003@gmail.com>
2024-02-26 21:35:29 -0600

    Change method name

--- frontend/clients/deleteItemsClient.jsx
+++ frontend/clients/deleteItemsClient.jsx
@@ -9,7 +9,7 @@ const deleteItemsClient = () => {
   const navigate = useNavigate()
   const { id } = useParams()
 
-  const handleDeleteBook = () => {
+  const handleDeleteItem = () => {
     setLoading(true)
     axios
     .delete(`http://localhost:5555/items/${id}`)
@@ -33,7 +33,7 @@ const deleteItemsClient = () => {
         <h3 className = 'text-2xl'> Comfirming delete this item...</h3>
         <button
           className = 'p-4 bg-red-600 text-white m-8 w-full'
-          onClick={handleDeleteBook}
+          onClick={handleDeleteItem}
         >
           Yes
         </button>
